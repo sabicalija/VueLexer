1a1505601971eb0bf06bd09cb26a2da4e86a6239
Alija Sabic <sabic.alija@gmail.com>
2022-11-13 03:05:53 +0100

    Refactor directory structure

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
-setup (
-  name='vuelexer',
-  packages=find_packages(),
-  entry_points =
-  """
-  [pygments.lexers]
-  vuelexer = vuelexer.lexer:VueLexer
-  """,
+setup(
+    name='vuelexer',
+    packages=find_packages(),
+    entry_points="""
+    [pygments.lexers]
+    vuelexer = vuelexer:VueLexer
+    """,
 )
