678e64084e499867dbe13c908b6cded5355226e8
Alija Sabic <sabic.alija@gmail.com>
2022-11-13 15:51:08 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='vuelexer',
+    version='0.0.1',
+    description='Custom Vue.js Pygments Lexer',
+    license='GPL-v3',
+    keywords='pygments lexer vue',
+    author='Alija Sabic',
+    author_email='sabic.alija@gmail.com',
     packages=find_packages(),
+    install_requires=['pygments >= 1.5'],
     entry_points="""
-    [pygments.lexers]
-    vuelexer = vuelexer:VueLexer
+        [pygments.lexers]
+        vuelexer = lexer.vuelexer:VueLexer
     """,
 )
